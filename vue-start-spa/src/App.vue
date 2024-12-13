c5cd7bb4de9219325281cfe1b3ff6c03f732e3d7
yuchiro <czarniknatalia16@gmail.com>
2024-12-13 23:17:45 +0100

    day 13
    vue learning

--- vue-start-spa/src/App.vue
+++ vue-start-spa/src/App.vue
@@ -4,17 +4,20 @@
     :active-page="activePage"
     :nav-link-click="(index) => (activePage = index)"
   ></navbar>
-  <page-view v-if="pages.length > 0" :page="pages[activePage]"></page-view>
+  <!-- <page-view v-if="pages.length > 0" :page="pages[activePage]"></page-view> -->
+  <create-page :page-created="pageCreated"></create-page>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
 import PageView from "./components/PageView.vue";
+import CreatePage from "./components/CreatePage.vue";
 
 export default {
   components: {
     Navbar,
     PageView,
+    CreatePage,
   },
   data() {
     return {
@@ -31,6 +34,9 @@ export default {
       let data = await res.json();
 
       this.pages = data;
+    },
+    pageCreated(pageObj) {
+      console.log(pageObj);
     },
   },
 };
---
+++ vue-start-spa/src/components/CreatePage.vue
@@ -0,0 +1,84 @@
+<template>
+  <form class="container mb-3">
+    <div class="row">
+      <div class="col-md-8">
+        <div class="mb-3">
+          <label for="" class="form-label"> Page Title </label>
+          <input type="text" class="form-control" v-model="pageTitle" />
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label"> Content </label>
+          <textarea
+            type="text"
+            class="form-control"
+            rows="5"
+            v-model="content"
+          ></textarea>
+        </div>
+      </div>
+      <div class="col">
+        <div class="mb-3">
+          <label for="" class="form-label">Link Text</label>
+          <input type="text" class="form-control" v-model="linkText" />
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label">Link URL</label>
+          <input type="text" class="form-control" v-model="linkUrl" />
+        </div>
+        <div class="row mb-3">
+          <div class="form-check">
+            <input type="checkbox" class="form-check-input" />
+            <label for="" class="form-check-label">Published</label>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mb-3">
+      <button
+        class="btn btn-primary"
+        @click.prevent="submitForm"
+        :disabled="isFormInvalid"
+      >
+        Create Page
+      </button>
+    </div>
+  </form>
+</template>
+<script>
+export default {
+  props: ["pageCreated"],
+  data() {
+    return {
+      pageTitle: "",
+      content: "",
+      linkText: "",
+      linkUrl: "",
+    };
+  },
+  computed: {
+    isFormInvalid() {
+      return (
+        !this.pageTitle || !this.content || !this.linkText || !this.linkUrl
+      );
+    },
+  },
+  methods: {
+    submitForm() {
+      if (!this.pageTitle || !this.content || !this.linkText || !this.linkUrl) {
+        alert("Fill out the form");
+        return;
+      }
+
+      this.pageCreated({
+        pageTitle: this.pageTitle,
+        content: this.content,
+        link: {
+          text: this.linkText,
+          url: this.linkUrl,
+        },
+      });
+    },
+  },
+};
+</script>
