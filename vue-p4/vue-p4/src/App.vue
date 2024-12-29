b093afa100f17cdd6f804835bb4ed85149ae345a
yuchiro <czarniknatalia16@gmail.com>
2024-12-29 16:18:50 +0100

    day 29
    design update

--- vue-p4/vue-p4/src/App.vue
+++ vue-p4/vue-p4/src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <navbar></navbar>
+  <div class="post-container">
+    <post-shortcut></post-shortcut>
+  </div>
 </template>
 <script>
 import Navbar from "./components/Navbar.vue";
+import PostShortcut from "./components/PostShortcut.vue";
 export default {
-  components: { Navbar },
+  components: { Navbar, PostShortcut },
 };
 </script>
+<style scoped>
+.post-container {
+  max-width: 1450px;
+  margin: 50px auto;
+  display: grid;
+  grid-template-columns: 1fr;
+  place-items: center;
+}
+</style>
--- vue-p4/vue-p4/src/components/PostShortcut.vue
+++ vue-p4/vue-p4/src/components/PostShortcut.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="container">
-    <h1></h1>
-    <p></p>
+    <span>author_name</span>
+    <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+      doloribus culpa rem inventore quae sit tenetur quia odio saepe ex...
+    </p>
   </div>
 </template>
+<style scoped>
+.container {
+  padding: 30px;
+  background-color: rgb(119, 110, 255);
+  box-shadow: 10px 10px rgb(199, 198, 255);
+  max-width: 800px;
+  cursor: pointer;
+  transition: scale 300ms;
+}
+.container:hover {
+  scale: 1.05;
+}
+
+.container > span {
+  padding: 0 10px;
+  font-weight: bold;
+  color: rgb(211, 206, 255);
+}
+
+.container > h1 {
+  color: white;
+  margin: 0 0 20px 0;
+}
+
+.container > p {
+  color: rgb(225, 224, 255);
+  padding-left: 20px;
+  border-left: 5px solid rgb(143, 141, 255);
+}
+</style>
